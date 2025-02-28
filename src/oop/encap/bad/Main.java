8cd2dbba214cf9f1e3ad89fac56e3467a350e048
Jongkyun <hjk326333@gmail.com>
2025-02-28 09:04:47 +0900

    Day05 - 정보 은닉과 캡슐화

---
+++ src/oop/encap/bad/Main.java
@@ -0,0 +1,13 @@
+package oop.encap.bad;
+
+public class Main {
+    public static void main(String[] args) {
+        MyBirth my = new MyBirth();
+        my.year = 2000;
+        my.month = 8;
+        my.day = 9;
+
+        my.birthInfo();
+
+    }
+}
